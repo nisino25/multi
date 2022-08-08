7c9254da85bc9b02ed9059899844cdea951bf151
nisino25 <nisino25@gmail.com>
2022-08-09 08:05:48 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <iframe src="http://localhost:8080/" frameborder="0"></iframe>
+  <iframe src="http://localhost:8080/" frameborder="0"></iframe>
+  <iframe src="http://localhost:8080/" frameborder="0"></iframe>
+  <hr>
+
+  <iframe src="http://localhost:8080/" frameborder="0"></iframe>
+  <iframe src="http://localhost:8080/" frameborder="0"></iframe>
+  <iframe src="http://localhost:8080/" frameborder="0"></iframe>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+
+iframe{
+  
+  width: auto;
+  height: 48vh;
+  margin-right: 20px
+}
+</style>
