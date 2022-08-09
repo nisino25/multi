0e3af7679d68588fa77283a35e13482e4226e533
nisino25 <nisino25@gmail.com>
2022-08-09 10:04:09 +0900

    good enough

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <iframe src="http://localhost:8080/" frameborder="0"></iframe>
   <iframe src="http://localhost:8080/" frameborder="0"></iframe>
   <iframe src="http://localhost:8080/" frameborder="0"></iframe>
-  <hr>
+  <!-- <hr> -->
+  <br>
 
   <iframe src="http://localhost:8080/" frameborder="0"></iframe>
   <iframe src="http://localhost:8080/" frameborder="0"></iframe>
@@ -31,9 +32,28 @@ export default {
 }
 
 iframe{
+  margin-top: 5px;
   
   width: auto;
-  height: 48vh;
-  margin-right: 20px
+  height: 49vh;
+  margin-right: 20px;
+
+
+  -ms-zoom: 0.9;
+  -moz-transform: scale(0.9);
+  -moz-transform-origin: 0 0;
+  -o-transform: scale(0.8);
+  -o-transform-origin: 0 0;
+  -webkit-transform: scale(0.9);
+  -webkit-transform-origin: 0 0;
+  
 }
+
+#wrap { width: 600px; height: 390px; padding: 0; overflow: hidden; }
+    #frame { width: 800px; height: 520px; border: 1px solid black; }
+    #frame {
+        
+    }
+
+
 </style>
